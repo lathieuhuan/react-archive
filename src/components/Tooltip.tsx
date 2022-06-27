63449b284a4ab740f178d79dea2470306f93288f
La Thieu Huan <huan.la@onemount.com>
2022-06-27 17:09:18 +0700

    make tooltip

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+
+type Placement = "top" | "bottom" | "right" | "left";
+
+interface TooltipProps {
+  className?: string;
+  text: string;
+  placement?: Placement;
+}
+export default function Tooltip(props: TooltipProps) {
+  const classNameByPlacement: Record<Placement, string> = {
+    top: "tooltip-top bottom-full left-0 right-0 mb-2 origin-[bottom_center] after:top-full after:left-1/2 after:-translate-x-1/2",
+    bottom: "",
+    right: "",
+    left: "",
+  };
+  return (
+    <span
+      className={classNames(
+        "absolute z-20 w-full px-4 py-2 rounded bg-black text-white scale-0 transition-transform duration-200 group-hover:scale-100 after:border-4 after:absolute",
+        classNameByPlacement[props.placement || "top"],
+        props.className
+      )}
+    >
+      {props.text}
+    </span>
+  );
+}
